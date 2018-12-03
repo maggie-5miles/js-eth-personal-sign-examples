01565bd511152205cf083d1a09fc6e5e487c6ea8
hewigovens <360470+hewigovens@users.noreply.github.com>
2018-12-03 13:52:19 +0800

    Fix logs

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ var terms = fs.readFileSync(__dirname + '/terms.txt').toString()
 
 requestAccountsButton.addEventListener('click', function(event) {
   window.ethereum.enable().then(function(result) {
-    console.log("result: " + JSON.stringify(result.result))
+    console.log("result: " + JSON.stringify(result))
   }).catch(function(error) {
     console.log(error)
   })
